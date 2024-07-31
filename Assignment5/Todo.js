b3902ee870424a668c4a4d7bb157f9ede3364d05
Vadla Swathi <swathivadla26@gmail.com>
2024-07-31 13:19:18 +0530

    New folders to push to GitHub

--- Assignment5/Todo.js
+++ Assignment5/Todo.js
@@ -1,46 +1,12 @@
-//DOMcontentLoaded -> It is an event, which refers to the completion of DOM loading in the browser
-// add_task_button - snake case notation
-// addTaskButton - camel case
-
-document.addEventListener('DOMContentLoaded',()=>{
-    const addTaskButton =document.getElementById('add-task-button');
-    const newTaskInput =document.getElementById('new-task');
-    const taskList =document.getElementById('task-list');
-
-    addTaskButton.addEventListener('click', addTask);
-    // IIFE -> immediately invoked function expression
-    newTaskInput.addEventListener('keypress',(event)=>{
-        if(event.key === "Enter"){
-            addTask();
-        }  
-    });
-    function addTask(){
-        const taskText = newTaskInput.value.trim();
-        if(taskText != ' '){
-            const listElement = document.createElement('li');
-            listElement.textContent = taskText;
-
-            const completeButton = document.createElement('button');
-            completeButton.textContent = 'Complete';
-            completeButton.addEventListener('click', ()=>{
-                listElement.classList.toggle('completed');
-                console.log(listElement);
-            })
-
-
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Delete';
-            deleteButton.classList.add('delete');
-
-            deleteButton.addEventListener('click', ()=>{
-                listElement.remove();
-            })
-
-            listElement.appendChild(completeButton);
-            listElement.appendChild(deleteButton);
-
-            taskList.appendChild(listElement);
-            newTaskInput.value="";
-        }
+function addTask() {
+    let taskInput = document.getElementById('new-task').value;
+    if (taskInput === '') {
+        alert("Please enter a task");
+        return;
     }
-})
+    let taskList = document.getElementById('task-list');
+    let li = document.createElement('li');
+    li.textContent = taskInput;
+    taskList.appendChild(li);
+    document.getElementById('new-task').value = '';
+}
